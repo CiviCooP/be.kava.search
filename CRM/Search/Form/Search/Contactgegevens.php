db385deab4256a523223ef67621a3292dc97ef9d
Alain Benbassat <alain@businessandcode.eu>
2017-11-14 11:25:02 +0100

    d

--- CRM/Search/Form/Search/Contactgegevens.php
+++ CRM/Search/Form/Search/Contactgegevens.php
@@ -111,7 +111,7 @@ class CRM_Search_Form_Search_Contactgegevens extends CRM_Contact_Form_Search_Cus
     // groothandel
     $from .= "
       LEFT OUTER JOIN
-        civicrm_relationship grooth_rel ON grooth_rel.contact_id_a = titu.id AND grooth.is_active = 1 and grooth_rel.relationship_type_id = $reltypeGroothandel
+        civicrm_relationship grooth_rel ON grooth_rel.contact_id_a = titu.id AND grooth_rel.is_active = 1 and grooth_rel.relationship_type_id = $reltypeGroothandel
       LEFT OUTER JOIN
         civicrm_contact grooth ON grooth_rel.contact_id_b = grooth.id
     ";
